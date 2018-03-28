e62c6bf5ed5d7ef6e0a96fb66192fead17ed6511
lzcdev <lzc2134@gmail.com>
2018-03-28 17:23:14 +0800

    封装网络请求+增加工具类功能

--- utils/network.js
+++ utils/network.js
@@ -1,4 +1,4 @@
-var baseUrl = "https://api.lujingkeji.com";
+var baseUrl = "https://api.lujingkeji.com/go_web";
 
 var requestHandler = {
   url: '',
@@ -32,7 +32,33 @@ function request(method, requestHandler) {
       'XBToken': 'lsOLXbNDThGyGGHGZx/pUL7zz6C6UlgOUOlAioNfah8='
     },
     success: function(res) {
-      requestHandler.success(res)
+      if (res.statusCode == 518) {
+
+        var lujing_error = JSON.parse(decodeURIComponent(JSON.stringify(res.header['lujing_error'])));
+        // console.log(lujing_error)
+
+        wx.showToast({
+          title: lujing_error,
+          icon: 'none',
+          duration: 2000
+        })
+      } else if (res.statusCode == 404) {
+        wx.showToast({
+          title: '资源未找到，请稍后重试',
+          icon: 'none',
+          duration: 2000
+        })
+      } else if (res.statusCode == 403) {
+        wx.showToast({
+          title: '登录过期，请重新登录',
+          icon: 'none',
+          duration: 2000
+        })
+      } else {
+        requestHandler.success(res)
+      }
+
+
     },
     fail: function(err) {
       requestHandler.fail(err)
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,80 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+// 获取系统信息
+const getSystemInfo = msg => {
+  var model, pixelRatio, windowWidth, windowHeight, language, version, platform, system
+      wx.getSystemInfo({
+      success: function (res) {
+        // console.log(res.model)
+        // console.log(res.pixelRatio)
+        // console.log(res.windowWidth)
+        // console.log(res.windowHeight)
+        // console.log(res.language)
+        // console.log(res.version)
+        // console.log(res.platform)
+        model = res.model
+        pixelRatio = res.pixelRatio
+        windowWidth = res.windowWidth
+        windowHeight = res.windowHeight
+        language = res.language
+        version = res.version
+        platform = res.platform
+        system = res.system
+
+      }
+    })
+  switch (msg) {
+    case 'model': return model; break;
+    case 'pixelRatio': return pixelRatio; break;
+    case 'windowWidth': return windowWidth; break;
+    case 'windowHeight': return windowHeight; break;
+    case 'language': return language; break;
+    case 'version': return version; break;
+    case 'platform': return platform; break;
+    case 'system': return system; break;
+  }  
+}
+
+// 获取系统信息同步接口(暂时写4个)
+const getSystemInfoSync = msg => {
+  var brand, model, system, platform
+  wx.getSystemInfoSync({
+    success: function (res) {
+      brand = res.brand
+      model = res.model
+      system = res.system
+      platform = res.platform
+    }
+  })
+  switch (msg) {
+    case 'brand': return brand; break;
+    case 'model': return model; break;
+    case 'system': return system; break;
+    case 'platform': return platform; break;
+  }
+}
+
+// 已登录
+const hasLogin = () => {
+  try {
+    var value = wx.getStorageSync('UserModel')
+    if (value) {
+      // Do something with return value
+      return true
+    } else {
+      return false
+    }
+  } catch (e) {
+    // Do something when catch error
+  }
+}
+
+
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  getSystemInfo: getSystemInfo,
+  getSystemInfoSync: getSystemInfoSync,
+  hasLogin: hasLogin
 }
